8ef8c166c680d3e3f162b1bea8027680876359f3
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-10-20 19:58:00 +0100

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: zelhajou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 03:55:53 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/10/20 04:55:55 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/10/20 18:18:11 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	int		n;
+	int		i;
 	char	*ptr;
 
 	i = 0;
 	ptr = (char *)s;
-	while(i < n)
+	while (i < n)
 	{
-		if(s[i] == (char)c)
-			return (&ptr[i])
+		if (ptr[i] == (char)c)
+			return (&ptr[i]);
 		i++;
 	}
 	return (0);
