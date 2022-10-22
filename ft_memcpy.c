94f9fb4412a4154107657f4efc873059334142cb
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-10-22 02:34:42 +0100

    memory functions in string.h

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,26 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zelhajou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 19:31:40 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/10/20 19:53:23 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/10/21 22:59:03 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	int		i;
+	size_t	i;
 	char	*s1;
 	char	*s2;
 
 	i = 0;
-	s1 = (char *)dest;
+	s1 = (char *)dst;
 	s2 = (char *)src;
+	if (s1 == NULL && s2 == NULL)
+		return (0);
 	while (i < n)
 	{
 		s1[i] = s2[i];
 		i++;
 	}
-	return (dest);
+	return (dst);
 }
