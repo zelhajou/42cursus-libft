7ebcaf8fa74a31e4804e7c1f0ba5b752240b90e1
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-10-27 19:15:01 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,27 +6,32 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:20:39 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/10/27 11:29:51 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/10/27 13:10:42 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	i;
-	int	r;
+	int			sign;
+	long long	i;
+	long long	r;
 
 	i = 0;
 	sign = 1;
 	r = 0;
-	while ((str[i] >= 7 && str[i] <= 13) || str[i] == 32
-		|| str[i] == '-' || str[i] == '+')
+	while ((str[i] >= 7 && str[i] <= 13) || str[i] == 32)
 	{
-		if (str[i] == '-')
-			sign *= -1;
-		else if (str[i] == '+')
-			sign *= 1;
 		i++;
+	}
+	if (str[i] == '-')
+	{
+			sign *= -1;
+			i++;
+	}
+	else if (str[i] == '+')
+	{
+			sign *= 1;
+			i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
