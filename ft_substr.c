4232ef93b12ef8ccd3e00b0d88f67f448740aab8
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-10-27 21:04:10 +0100

    Create ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 15:41:59 by zelhajou          #+#    #+#             */
+/*   Updated: 2022/10/27 17:21:59 by zelhajou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	size_t			j;
+	char			*substr;
+
+	i = 0;
+	j = 0;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!s || !substr)
+		return (NULL);
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			substr[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	substr[j] = '\0';
+	return (substr);
+}
