5e93775ba43cd090a511013b0fa88285cd7a89c3
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-01 14:13:15 +0100

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 15:03:07 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/10/27 15:26:51 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/11/01 11:08:15 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 char	*ft_strdup(const char *src)
 {
 	int		i;
-	char	*strcpy;
+	char	*str;
 
 	i = 0;
-	strcpy = malloc(ft_strlen(src) + 1);
-	if (strcpy == NULL)
+	str = malloc(ft_strlen(src) + 1);
+	if (str == NULL)
 		return (NULL);
 	while (src[i])
 	{
-		strcpy[i] = src[i];
+		str[i] = src[i];
 		i++;
 	}
-	strcpy[i] = '\0';
-	return (strcpy);
+	str[i] = '\0';
+	return (str);
 }
