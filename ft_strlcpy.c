38e5bd2dd6a560d5a41cd17e45c31728340fa069
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-02 17:42:49 +0100

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,25 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zelhajou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 22:29:54 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/10/20 23:16:13 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/11/02 16:02:01 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	len;
-	char	*s1;
-	char	*s2;
 
 	i = 0;
-	s1 = (char *)dst;
-	s2 = (char *)src;
-	len = ft_strlen(s2);
+	len = ft_strlen(src);
 	if (size == 0)
 		return (len);
 	while (src[i] != '\0' && i < size - 1)
