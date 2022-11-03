8b13f90969f224a8f4a48353cb8f934b70941eba
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-03 17:44:51 +0100

    fix norminette

--- libft.h
+++ libft.h
@@ -33,7 +33,7 @@ int		ft_toupper(int c);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char    *ft_strnstr(const char    *big, const char *little, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
