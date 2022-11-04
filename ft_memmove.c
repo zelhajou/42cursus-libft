aa6a40c6412b1fa4c3df41f8106e2da25d3cc40d
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-04 17:18:15 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,18 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 22:55:32 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/11/04 03:14:36 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/11/04 17:16:39 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+	ft_memmove() copies len bytes from string src to string dst. The two strings 
+	may overlap; the copy is always done in a non-destructive manner.
+*/
+
 #include "libft.h"
 
-void    *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {	
 	char	*s1;
 	char	*s2;
