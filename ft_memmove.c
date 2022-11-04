aa0d5228736b6c9559d16abcc1e3526cff0aa27b
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-04 16:20:24 +0100

    add function

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,10 +6,25 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 22:55:32 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/10/21 23:46:25 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/11/04 03:14:36 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void    *ft_memmove(void *dst, const void *src, size_t len)
+void    *ft_memmove(void *dst, const void *src, size_t len)
+{	
+	char	*s1;
+	char	*s2;
+
+	s1 = (char *)src;
+	s2 = (char *)dst;
+	if (dst <= src)
+		dst = ft_memcpy(dst, src, len);
+	else
+	{
+		while (len--)
+			s2[len] = s1[len];
+	}
+	return (dst);
+}
