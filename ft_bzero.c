70757944d53cbcd006f2e65384d7cbf248626625
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-04 16:20:33 +0100

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,16 +6,14 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 04:42:09 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/11/03 02:29:33 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/11/03 22:57:53 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-
 	ft_bzero() erases the data in the n bytes of the memory starting at 
 	the location pointed to by s, by writing zeros (bytes containing '\0') 
 	to that area.
-
 */
 
 #include "libft.h"
