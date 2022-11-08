4383464231e37562970c704c7acf671f9d8e8155
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-08 19:20:01 +0100

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,27 +6,25 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 01:03:10 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/11/06 01:49:33 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/11/07 17:40:14 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 // char	f(unsigned int s, char c)
 // {
 // 	return (c + s);
 // }
-
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	int		i;
 	char	*str;
-	
+
 	i = 0;
-	str = malloc(sizeof(char) * strlen(s) + 1);
+	str = malloc(sizeof(char) * ft_strlen(s) + 1);
 	if (!str)
 		return (NULL);
-	while(s[i])
+	while (s[i])
 	{
 		str[i] = f(i, s[i]);
 		i++;
