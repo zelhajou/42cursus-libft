1168b3442a5bba0d6931a7686bdefd40f68aa8c8
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-13 01:30:16 +0100

    add function description and change the data type of variables to size_t

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,16 +6,21 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 15:03:07 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/11/04 00:32:08 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/11/13 01:28:54 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+	strdup() returns a pointer to a new string which is a duplicate 
+	of the string s.  Memory for the new string is obtained with malloc.
+*/
 
 #include "libft.h"
 
 char	*ft_strdup(const char *src)
 {
-	int		i;
-	int		size;
+	size_t	i;
+	size_t	size;
 	char	*str;
 
 	i = 0;
