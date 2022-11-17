87219e7c39458b681361e5b2dd78e38b00a41c88
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-17 12:13:19 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -68,5 +68,14 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
+/* Bonus Part */
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
