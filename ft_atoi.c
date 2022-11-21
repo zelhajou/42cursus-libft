6d27dd1758f43347e24b36c7799aa44d60f24b0a
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-21 23:12:48 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:20:39 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/11/19 20:42:14 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/11/21 23:11:44 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,45 @@
 
 #include "libft.h"
 
+int	skip_space(int *i, const char *str)
+{
+	int	sign;
+
+	sign = 1;
+	while ((str[*i] >= 9 && str[*i] <= 13) || str[*i] == 32)
+		(*i)++;
+	if (str[*i] == '-' || str[*i] == '+')
+	{
+		if (str[*i] == '-')
+			sign *= -1;
+		(*i)++;
+	}
+	return (sign);
+}
+
 int	ft_atoi(const char *str)
 {
-	int		sign;
-	int		i;
-	long	res;
+	int			sign;
+	int			i;
+	long long	res;
+	long long	prev;
+	long long	tmp;
 
 	i = 0;
-	sign = 1;
 	res = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
+	sign = skip_space(&i, str);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
+		prev = res;
 		res = res * 10 + str[i] - '0';
+		tmp = res / 10;
+		if (tmp != prev)
+		{
+			if (sign == 1)
+				return (-1);
+			else
+				return (0);
+		}
 		i++;
 	}
 	return (sign * res);
