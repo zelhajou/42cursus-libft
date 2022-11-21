f6d1434d1cf9575284752a262e968e15d8c19f86
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-11-21 23:19:58 +0100

    delete condition

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 19:31:40 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/11/19 20:42:52 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/11/21 03:30:39 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,16 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char	*s2;
 
 	i = 0;
+	if (!dst && !src)
+		return (0);
 	s1 = (unsigned char *)dst;
 	s2 = (unsigned char *)src;
-	if (s1 == NULL && s2 == NULL)
-		return (0);
+	if (s1 == s2)
+		return (s1);
 	while (i < n)
 	{
 		s1[i] = s2[i];
 		i++;
 	}
-	return (dst);
+	return (s1);
 }
